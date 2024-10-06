bd967acb12a8ab53b48d063208ae90fb56514828
Ilya <ilyasichev2015@gmail.com>
2024-10-07 01:02:24 +0300

    add new popup for material in lesson and task

--- public/js/taskPopup.js
+++ public/js/taskPopup.js
@@ -1,7 +1,17 @@
 const tintDiv = document.querySelector(".tint");
+const matTintDiv = document.querySelector(".material-tint");
+const matTaskDiv = document.querySelector(".sidebar-links-task-material");
 const closeBtnDiv = document.querySelector(".close-btn");
+const closeMaterialBtnDiv = document.querySelector(".close-material-btn");
+
+matTaskDiv.addEventListener("click", showMaterialPopup)
 
 closeBtnDiv.addEventListener("click", closePopup);
+closeMaterialBtnDiv.addEventListener("click", closeMaterialPopup);
+
+matTintDiv.addEventListener("click", (e) =>{
+	if (e.target.classList.contains("material-tint")) closeMaterialPopup();
+});
 
 tintDiv.addEventListener("click", (e) => {
 	if (e.target.classList.contains("tint")) closePopup();
@@ -15,9 +25,17 @@ document.addEventListener("click", (e) => {
 });
 
 function showPopup() {
-	tintDiv.classList.remove("tint_hidden");
+	tintDiv.classList.remove("tint-hidden");
 }
 
 function closePopup() {
-	tintDiv.classList.add("tint_hidden");
+	tintDiv.classList.add("tint-hidden");
+}
+
+function showMaterialPopup() {
+	matTintDiv.classList.remove("material-tint-hidden");
+}
+
+function closeMaterialPopup() {
+	matTintDiv.classList.add("material-tint-hidden");
 }
