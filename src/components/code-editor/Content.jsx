8c100d9f5d325a6b9b7abe22c7e34acfed4ce957
konditer <konditer1324@gmail.com>
2024-10-13 00:41:08 +0300

    feat: code editor base

--- src/components/code-editor/Content.jsx
+++ src/components/code-editor/Content.jsx
@@ -1,5 +1,49 @@
+import { Rnd } from "react-rnd";
+import "./Content.css";
+
 const CodeEditorContent = () => {
-    return <div>code content</div>;
-}
+	return (
+		<div className="code-container">
+			<Rnd
+				bounds="parent"
+				className="code-item"
+				size={{ width: "100%", height: 70 }}
+				dragGrid={[1, 70]}
+				dragAxis="y"
+				enableResizing={{
+					bottom: false,
+					bottomLeft: false,
+					bottomRight: false,
+					left: false,
+					right: false,
+					top: false,
+					topLeft: false,
+					topRight: false,
+				}}
+			>
+				<div className="code-item-inner">1</div>
+			</Rnd>
+			<Rnd
+				bounds="parent"
+				className="code-item"
+				size={{ width: "100%", height: 70 }}
+				dragGrid={[1, 70]}
+				dragAxis="y"
+				enableResizing={{
+					bottom: false,
+					bottomLeft: false,
+					bottomRight: false,
+					left: false,
+					right: false,
+					top: false,
+					topLeft: false,
+					topRight: false,
+				}}
+			>
+				<div className="code-item-inner">2</div>
+			</Rnd>
+		</div>
+	);
+};
 
 export default CodeEditorContent;
