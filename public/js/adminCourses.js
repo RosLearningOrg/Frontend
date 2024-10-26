454477db0f07afc16319755d98fe8623d9ee7b3b
konditer <konditer1324@gmail.com>
2024-10-26 15:13:14 +0300

    fix: auth and courses

---
+++ public/js/adminCourses.js
@@ -0,0 +1,45 @@
+import { API_URL, logout } from "/js/main.js";
+
+const contentContainer = document.getElementsByClassName("main-content")[0];
+
+const getAllCourses = async () => {
+	const init = {
+		method: "GET",
+		credentials: "include",
+	};
+
+	try {
+		const resp = await fetch(API_URL + "/admin/getAllCourses", init);
+		const data = await resp.json();
+		setContent(data);
+	} catch {
+		logout();
+	}
+};
+
+const setContent = (data) => {
+	let content = "";
+
+	for (let item of data) {
+		content += `
+            <div href="lessons.html" class="course-item-container" draggable="false">
+                <div class="course-item-info">
+                    <p class="course-item-title">${item.title}</p>
+                    <p class="course-item-desc hint">${item.description}</p>
+                </div>
+
+                <div class="icon-container view-personal-icon">
+                    <img class="course-item-personal-icon" src="./images/fluent_people-28-regular.svg" alt="">
+                </div>
+                <div class="icon-container open-dropdown-icon">
+                    <img class="course-item-dots-icon" src="./images/Vector.svg" alt="">
+                </div>
+            </div>
+        `;
+	}
+	contentContainer.innerHTML = content;
+};
+
+(async () => {
+	await getAllCourses();
+})();
--- public/js/courses.js
+++ public/js/courses.js
@@ -4,10 +4,13 @@ const getUserCourses = async () => {
 	const init = {
 		method: "GET",
 		credentials: "include",
+		headers: {
+			"X-CSRF-TOKEN": sessionStorage.getItem("csrf"),
+		},
 	};
 
 	try {
-        const resp = await fetch(API_URL + "/getUserCourses", init)
+        const resp = await fetch(API_URL + "/user/getUserCourses", init)
         const data = await resp.json();
         console.log(data)
 	} catch {
