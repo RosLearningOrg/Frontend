c5976ab0f0222b0e1d144c916d685e2e1baef15d
konditer <konditer1324@gmail.com>
2024-10-28 16:18:18 +0300

    feat: implemented code variables

--- src/components/code-editor/Sidebar.jsx
+++ src/components/code-editor/Sidebar.jsx
@@ -1,13 +1,13 @@
 import "./Sidebar.css";
-import { useContext, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import { CodeContext } from "../../context/codeContext";
 import { codeItemsTypes } from "../../code/itemsTypes";
 
 const CodeEditorSidebar = () => {
 	const { addItem, variables, setVar, removeVar } = useContext(CodeContext);
 	const [selected, setSelected] = useState(0);
-
-    console.log(variables)
+	const [creatingVar, setCreatingVar] = useState(false);
+	const varNameInput = useRef(null);
 
 	return (
 		<>
@@ -31,7 +31,11 @@ const CodeEditorSidebar = () => {
 			>
 				{codeItemsTypes.map((item, index) => {
 					return (
-						<button key={index} onClick={() => addItem(item)}>
+						<button
+							data-variant="tonal"
+							key={index}
+							onClick={() => addItem(item)}
+						>
 							{item.title}
 						</button>
 					);
@@ -41,12 +45,60 @@ const CodeEditorSidebar = () => {
 				className="code-sidebar-inner"
 				data-state={selected == 1 ? "visible" : "hidden"}
 			>
-                {Object.entries(variables).map(([name, value], index) => {
-                    return (
-                        <div key={index}>{name}, {value}</div>
-                    )
-                })}
-                <button onClick={() => {setVar("ok", "ok")}}></button>
+				{Object.entries(variables).map(([name, value], index) => {
+					return (
+						<div key={index} className="var-container">
+							<img
+								src="/images/delete.svg"
+								alt="photo"
+								className="remove-var-bttn"
+								onClick={() => {
+									removeVar(name);
+								}}
+							/>
+							<p>Название:</p>
+							<p>{name}</p>
+							<p>Значение:</p>
+							<input
+								type="number"
+								value={value}
+								onChange={(e) => setVar(name, e.target.value)}
+							/>
+						</div>
+					);
+				})}
+				{!creatingVar && (
+					<button
+						data-variant="square"
+						onClick={() => {
+							setCreatingVar(true);
+						}}
+					>
+						<img src="/images/plus.svg" alt="photo" />
+						Создать перменную
+					</button>
+				)}
+				{creatingVar && (
+					<div className="creating-var-container">
+						<img
+							onClick={() => setCreatingVar(false)}
+							src="/images/plus.svg"
+							alt="photo"
+							className="close-creating-var"
+						/>
+						<p>Название переменной</p>
+						<input type="text" ref={varNameInput} />
+						<button
+							data-variant="primary"
+							onClick={() => {
+								setVar(varNameInput.current.value, 0);
+								setCreatingVar(false);
+							}}
+						>
+							Создать
+						</button>
+					</div>
+				)}
 			</div>
 		</>
 	);
