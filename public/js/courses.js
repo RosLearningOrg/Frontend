c4f257e8e6ccbe810f9ead3e6ecb84990d933451
Michael <michaelkivo977@gmail.com>
2024-10-29 12:53:08 +0300

    Implement getUserCourses()

--- public/js/courses.js
+++ public/js/courses.js
@@ -1,22 +1,49 @@
 import { API_URL } from "/js/main.js";
 
+const contentContainer = document.getElementsByClassName("main-content")[0];
+
 const getUserCourses = async () => {
 	const init = {
 		method: "GET",
-		credentials: "include",
-		headers: {
-			"X-CSRF-TOKEN": sessionStorage.getItem("csrf"),
-		},
+		credentials: "include"
 	};
 
 	try {
         const resp = await fetch(API_URL + "/user/getUserCourses", init)
         const data = await resp.json();
         console.log(data)
+		setContent(data)
 	} catch {
         return null;
 	}
 };
+
+const setContent = (data) => {
+	let content = "";
+
+	for (let item of data) {
+		content += `
+            <div href="lessons.html" class="course-item-container" draggable="false" data-course-id=${item.id}>
+                <div class="course-item-info">
+                    <p class="course-item-title">${item.title}</p>
+                    <p class="hint course-item-desc">${item.description}</p>
+                </div>
+                 <p class="course-item-stats">2 / 15</p>
+            </div>
+        `;
+	}
+	contentContainer.innerHTML = content;
+};
+
+document.addEventListener("click", (e) => {
+    const courseItem = e.target.closest(".course-item-container");
+
+    if (courseItem) {
+        const id = courseItem.getAttribute("data-course-id")
+        sessionStorage.setItem("course_id", id)
+		window.location.href = window.location.origin + "/lessons.html";
+    }
+});
 
 (async () => {
     await getUserCourses();
