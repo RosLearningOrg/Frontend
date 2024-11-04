1bc406cd6b687813441e032a0d3f325da6935e99
Michael <michaelkivo977@gmail.com>
2024-11-04 16:57:11 +0300

    Add popup with select materials

--- public/js/tasksPopups.js
+++ public/js/tasksPopups.js
@@ -1,12 +1,23 @@
 const taskPopupTint = document.querySelector(".task-popup-tint");
-const materialPopupTint = document.querySelector(".material-popup-tint");
+const materialPopupSelectTint = document.querySelector(".material-popup-tint-select");
 const taskPopupCloseBtn = document.querySelector(".task-close-btn");
 const materialPopupCloseBtn = document.querySelector(".material-close-btn");
 const materialOpenBtn = document.querySelector(".material-open-btn");
+const selectButton = document.querySelector(".text-box-select");
+const materialPopupTint = document.querySelector(".material-popup-tint");
+const backButton = document.querySelector(".back_popup-image");
+const materialCloseButton = document.querySelector(".material-close-btn-end");
 
-materialOpenBtn.addEventListener("click", showMaterialPopup)
+materialOpenBtn.addEventListener("click", showMaterialSelectPopup);
 taskPopupCloseBtn.addEventListener("click", closeTaskPopup);
-materialPopupCloseBtn.addEventListener("click", closeMaterialPopup);
+materialPopupCloseBtn.addEventListener("click", closeMaterialSelectPopup);
+materialCloseButton.addEventListener("click", (e) => {
+	closeMaterialPopup();
+});
+
+materialPopupSelectTint.addEventListener("click", (e) =>{
+	if (e.target.classList.contains("material-popup-tint-select")) closeMaterialSelectPopup();
+});
 
 materialPopupTint.addEventListener("click", (e) =>{
 	if (e.target.classList.contains("material-popup-tint")) closeMaterialPopup();
@@ -29,6 +40,14 @@ function closeTaskPopup() {
 	taskPopupTint.classList.add("popup-tint-hidden");
 }
 
+function showMaterialSelectPopup() {
+	materialPopupSelectTint.classList.remove("popup-tint-hidden");
+}
+
+function closeMaterialSelectPopup() {
+	materialPopupSelectTint.classList.add("popup-tint-hidden");
+}
+
 function showMaterialPopup() {
 	materialPopupTint.classList.remove("popup-tint-hidden");
 }
@@ -36,3 +55,13 @@ function showMaterialPopup() {
 function closeMaterialPopup() {
 	materialPopupTint.classList.add("popup-tint-hidden");
 }
+
+selectButton.addEventListener("click", (e) => {
+    closeMaterialSelectPopup();
+	showMaterialPopup();
+});
+
+backButton.addEventListener("click", (e) => {
+    closeMaterialPopup();
+	showMaterialSelectPopup();
+});
