d3a8a444507556c0e1292aa65a41f091c3595ec0
konditer <konditer1324@gmail.com>
2024-11-12 00:56:22 +0300

    fix: change editor context scope

--- src/routes/Editor.jsx
+++ src/routes/Editor.jsx
@@ -1,4 +1,4 @@
-import "./Editor.css"
+import "./Editor.css";
 import { useState } from "react";
 import CodeEditorContent from "../components/code-editor/Content";
 import CodeEditorLeftSidebar from "../components/code-editor/LeftSidebar";
@@ -10,58 +10,58 @@ import CodeContextProvider from "../context/codeContext";
 import EditorContextProvider from "../context/editorContext";
 
 const Editor = () => {
-    const [selected, setSelected] = useState(0);
-    const switchSelected = (index) => setSelected(index);
+	const [selected, setSelected] = useState(0);
+	const switchSelected = (index) => setSelected(index);
 
-    return (
-        <div className="editor-layout">
-            <header className="editor-header">
-                <button
-                    onClick={() => switchSelected(0)}
-                    data-variant={selected == 0 ? "primary" : "tonal"}
-                >
-                    Редактировать код
-                </button>
-                <button
-                    onClick={() => switchSelected(1)}
-                    data-variant={selected == 1 ? "primary" : "tonal"}
-                >
-                    Редактировать интерфейс
-                </button>
-            </header>
-            <EditorContextProvider>
-                <InterfaceContextProvider>
-                    <div
-                        className="editor-wrapper"
-                        data-state={selected == 1 ? "visible" : "hidden"}
-                    >
-                        <aside className="editor-sidebar" data-pos="left">
-                            <InterfaceEditorSidebar />
-                        </aside>
-                        <main className="editor-content">
-                            <InterfaceEditorContent />
-                        </main>
-                    </div>
-                </InterfaceContextProvider>
-                <CodeContextProvider>
-                    <div
-                        className="editor-wrapper"
-                        data-state={selected == 0 ? "visible" : "hidden"}
-                    >
-                        <aside className="editor-sidebar" data-pos="left">
-                            <CodeEditorLeftSidebar />
-                        </aside>
-                        <main className="editor-content">
-                            <CodeEditorContent />
-                        </main>
-                        <main className="editor-sidebar" data-pos="right">
-                            <CodeEditorRightSidebar />
-                        </main>
-                    </div>
-                </CodeContextProvider>
-            </EditorContextProvider>
-        </div>
-    );
+	return (
+		<div className="editor-layout">
+			<EditorContextProvider>
+				<header className="editor-header">
+					<button
+						onClick={() => switchSelected(0)}
+						data-variant={selected == 0 ? "primary" : "tonal"}
+					>
+						Редактировать код
+					</button>
+					<button
+						onClick={() => switchSelected(1)}
+						data-variant={selected == 1 ? "primary" : "tonal"}
+					>
+						Редактировать интерфейс
+					</button>
+				</header>
+				<InterfaceContextProvider>
+					<div
+						className="editor-wrapper"
+						data-state={selected == 1 ? "visible" : "hidden"}
+					>
+						<aside className="editor-sidebar" data-pos="left">
+							<InterfaceEditorSidebar />
+						</aside>
+						<main className="editor-content">
+							<InterfaceEditorContent />
+						</main>
+					</div>
+				</InterfaceContextProvider>
+				<CodeContextProvider>
+					<div
+						className="editor-wrapper"
+						data-state={selected == 0 ? "visible" : "hidden"}
+					>
+						<aside className="editor-sidebar" data-pos="left">
+							<CodeEditorLeftSidebar />
+						</aside>
+						<main className="editor-content">
+							<CodeEditorContent />
+						</main>
+						<main className="editor-sidebar" data-pos="right">
+							<CodeEditorRightSidebar />
+						</main>
+					</div>
+				</CodeContextProvider>
+			</EditorContextProvider>
+		</div>
+	);
 };
 
 export default Editor;
